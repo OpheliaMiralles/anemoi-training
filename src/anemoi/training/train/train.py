a09eb4be0de0dcff9ca9dc44662126cc25ffefe3
omiralles <ophelia.miralles@meteoswiss.ch>
2025-01-06 11:48:28 +0100

    Remove unused import

--- src/anemoi/training/train/train.py
+++ src/anemoi/training/train/train.py
@@ -34,7 +34,6 @@ from anemoi.training.diagnostics.logger import get_mlflow_logger
 from anemoi.training.diagnostics.logger import get_tensorboard_logger
 from anemoi.training.diagnostics.logger import get_wandb_logger
 from anemoi.training.distributed.strategy import DDPGroupStrategy
-from anemoi.training.train.forecaster import GraphForecaster
 from anemoi.training.utils.checkpoint import transfer_learning_loading
 from anemoi.training.utils.jsonify import map_config_to_primitives
 from anemoi.training.utils.seeding import get_base_seed
